080c6276c4651e630f465ea5ac6a373a0bb8ad9d
crbanks1 <chadrbanks@yahoo.com>
2020-01-23 18:45:10 -0500

    Worked on first JS examples, we have console.logs!

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # cis-172-examples
 
+Author: Chad Banks
+
 THis project is managed by Prof. Banks.
 
 Contact me at chadrbanks@yahoo.com
--- test.js
+++ test.js
@@ -1,7 +1,17 @@
 
 
-console.log(123);
+let favoriteMovie = "The Matrix";
 
+let hello = "Hello";
+let world = "World";
+let space = " ";
 
+//console.log("Hello world!");
 
-// asfboiuaodf
+console.log( hello + space + world );
+
+console.log(42);
+
+console.log(favoriteMovie);
+
+console.error("This is an error!");
